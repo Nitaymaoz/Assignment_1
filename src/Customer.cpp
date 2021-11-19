2c36057d8b48c1016ea53b266f7e8f05d6e6f84f
GuyTurevich <guy938@gmail.com>
2021-11-19 16:43:02 +0200

    Customer.cpp changes

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -30,11 +30,14 @@ std::vector<int> SweatyCustomer::order(const std::vector <Workout> &workout_opti
 
 std::vector<int> CheapCustomer::order(const std::vector <Workout> &workout_options) {
     std::vector<int> workout_plan;
-    int minPrice=workout_options[0].getPrice();
-    int minId=workout_options[0].getId();
+    int minPrice = workout_options[0].getPrice();
+    int minId = workout_options[0].getId();
     for (Workout workout: workout_options) {
-        if (workout.getPrice()<minPrice)
-            minId=workout.getId();
+        if (workout.getPrice() < minPrice) {
+            minId = workout.getId();
+            minPrice = workout.getPrice();
+        }
+
     }
     workout_plan.insert(minId);
 
@@ -44,17 +47,18 @@ std::vector<int> CheapCustomer::order(const std::vector <Workout> &workout_optio
 //Heavy Muscle Customer
 
 std::vector<int> HeavyMuscleCustomer::order(const std::vector <Workout> &workout_options) {
-    std::vector<std::pair<int,int>> workout_plan; // first field is price second is ID
-    for(Workout workout : workout_options){
-        if (workout.getType()==0) // get all the anaerobic workouts
-            workout_plan.push_back(std::make_pair(workout.getPrice(),workout.getId()));
+    std::vector <std::pair<int, int>> workout_plan; // first field is price second is ID
+    for (Workout workout: workout_options) {
+        if (workout.getType() == 0) // get all the anaerobic workouts
+            workout_plan.push_back(std::make_pair(workout.getPrice(), workout.getId()));
     }
     std::vector<int> workout_plan_output;
     if (workout_plan.empty()) return workout_plan_output; //if there are none anaerobic workouts
 
-    std::stable_sort(workout_plan.begin(),workout_plan.end()); // sort from the cheapest to the most expensive . the reason to include <bits/stdc++.h>
-    for(std::pair<int,int> id:workout_plan){
-        workout_plan_output.push_back(id.second);
+    std::stable_sort(workout_plan.begin(),
+                     workout_plan.end()); // sort from the cheapest to the most expensive . the reason to include <bits/stdc++.h>
+    for (std::pair<int, int> workout: workout_plan) {
+        workout_plan_output.push_back(workout.second);
     }
     return workout_plan_output;
 }
@@ -63,23 +67,31 @@ std::vector<int> HeavyMuscleCustomer::order(const std::vector <Workout> &workout
 //Full Body Customer
 
 std::vector<int> FullBodyCustomer::order(const std::vector <Workout> &workout_options) {
-    std::vector<std::pair<int,int>> sort_cardio;
-    std::vector<std::pair<int,int>> sort_anaerobic;
-    std::vector<std::pair<int,int>> sort_mix;
-    for(Workout workout : workout_options){
-        if (workout.getType()==0) sort_anaerobic.push_back(std::make_pair(workout.getPrice(),workout.getId()))
-        else if (workout.getType()==1) sort_mix.push_back(std::make_pair(workout.getPrice(),workout.getId()))
-        else sort_anaerobic.push_back(std::make_pair(workout.getPrice(),workout.getId()))
+    std::vector <std::pair<int, int>> sort_cardio;
+    std::vector <std::pair<int, int>> sort_anaerobic;
+    std::vector <std::pair<int, int>> sort_mix;
+    for (Workout workout: workout_options) {
+        if (workout.getType() == 0) sort_anaerobic.push_back(std::make_pair(workout.getPrice(), workout.getId()))
+        else if (workout.getType() == 1) sort_mix.push_back(std::make_pair(workout.getPrice(), workout.getId()))
+        else sort_anaerobic.push_back(std::make_pair(workout.getPrice(), workout.getId()))
     }
     std::vector<int> workout_plan;
     if (sort_cardio.empty() || sort_mix.empty() || sort_anaerobic.empty()) return workout_plan;
 
-    std::stable_sort(sort_cardio.begin(),workout_plan.end());
-    std::stable_sort(sort_anaerobic.begin(),workout_plan.end());
-    std::stable_sort(sort_mix.begin(),workout_plan.end());
-    workout_plan.push_back(sort_cardio.begin()->second);    //id of cheapest cardio
-    workout_plan.push_back(sort_mix.end()->second);         // id of most expensive mix
-    workout_plan.push_back(sort_anaerobic.begin()->second); // id of cheapest anaerobic
+    std::stable_sort(sort_cardio.begin(), workout_plan.end());
+    std::stable_sort(sort_anaerobic.begin(), workout_plan.end());
+    std::stable_sort(sort_mix.begin(), workout_plan.end());
+
+    workout_plan.push_back(sort_anaerobic.begin()->second);  // id of the cheapest anaerobic
+
+    int highestMixPrice = sort_mix.end()->second;
+    for (int i = sort_mix.size() - 2; i >= 0; i--) {
+        if (sort_mix[i].first != highestMixPrice)
+            workout_plan.push_back(sort_mix[i + 1]->second); // id of most expensive mix (with the smallest id)
+    }
+
+    workout_plan.push_back(sort_cardio.begin()->second);     //id of cheapest cardio
+
     return workout_plan;
 }
 
